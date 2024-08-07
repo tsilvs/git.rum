d402506b973d2b71fae0250ca0297a32fc0db1b2
Vsevolod Tsiliurik <tsilvs@proton.me>
2024-08-07 21:36:08 +0100

    Creating more repos base commands

--- remote.init.sh
+++ remote.init.sh
@@ -18,19 +18,17 @@ REPO=""
 DESCR=""
 BRANCH="main"
 
-response=$(curl -L \
-  -X POST \
-  -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer $MYTOKEN_GH" \
-  -H "X-GitHub-Api-Version: 2022-11-28" \
-  "https://api.github.com/repos/$TPLOWNER/$TPLREPO/generate" \
-  -d '{"owner":"$OWNER","name":"$REPO","description":"$DESCR","include_all_branches":false,"private":false}')
+response=$(curl -X POST \
+	-H "Authorization: token $MYTOKEN_GH" \
+	-H "Accept: application/vnd.github.v3+json" \
+	"https://api.github.com/user/repos" \
+  -d '{"name":"$REPO","description":"$DESCR","private":false}')
 
 # + GitLab
 
 response=$(curl --header "PRIVATE-TOKEN: $MYTOKEN_GL" \
-     --data "name=$REPO" \
-     "https://gitlab.com/api/v4/projects")
+	--data "name=$REPO&description=$DESCR" \
+	"https://gitlab.com/api/v4/projects")
 
 project_url=$(echo $response | jq -r '.http_url_to_repo')
 git remote add $REM_GL $project_url
@@ -38,3 +36,8 @@ git push -u $REM_GL $BRANCH
 
 # + Codeberg
 
+response=$(curl -X POST \
+	-H "Authorization: token $MYTOKEN_CB" \
+	-H "Content-Type: application/json" \
+	"https://codeberg.org/api/v1/user/repos" \
+	-d '{"name":"$REPO", "description":"$DESCR", "private":false}')
